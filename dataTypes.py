2fe8294f348ab041d4e5891b71f47b1eb985275b
Manish Kumar <mk772542@gmail.com>
2025-11-24 22:35:54 +0530

    day 1

---
+++ dataTypes.py
@@ -0,0 +1,23 @@
+# first
+age = 25
+name = "Alice"
+height = 5.7
+is_student = False
+# second
+print("Age:", age)
+print("Name:", name)
+print("Height:", height)
+print("Is Student:", is_student)  # third
+print(type(age))
+print(type(name))
+print(type(height))
+print(type(is_student))
+
+# Performing basic operations
+age_next_year = age + 1
+height_in_cm = height * 30.48  # converting feet to cm
+is_graduate = not is_student
+print("Age next year:", age_next_year)
+print("Height in cm:", height_in_cm)
+print("Is Graduate:", is_graduate)  # first
+age = 25
