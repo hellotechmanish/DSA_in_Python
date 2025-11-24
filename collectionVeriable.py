a307d24b569b65f7b3eabc48edf940554528e218
Manish Kumar <mk772542@gmail.com>
2025-11-24 22:46:00 +0530

    this is collection veriable

---
+++ collectionVeriable.py
@@ -0,0 +1,26 @@
+# list
+mark = [22, 55, 5, 555, 44, 66, 77, 88, 99, 11]
+print(mark)
+print(mark[5])
+mark[2] = 777
+print(mark)
+
+# touple
+tuple = (22, 55, 5, 555, 44, 66, 77, 88, 99, 11)
+print(tuple)
+print(tuple[5])
+
+# set
+myset = {22, 55, 5, 555, 555, 44, 66, 77, 88, 99, 11}
+print("myset", myset)
+
+# none type
+a = None
+print(a)
+
+print(type(a))
+name = "Alice"
+print(type(name))
+
+complex_num = 2 + 3j
+print(type(complex_num))
